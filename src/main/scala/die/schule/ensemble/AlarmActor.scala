9e791db12ac0759a20ac881e07635e819b0d76d1
Sergio Magnacco <smagnacco@despegar.com>
2021-06-19 21:18:12 -0300

    Adding some service call for zipkin

--- src/main/scala/die/schule/ensemble/AlarmActor.scala
+++ src/main/scala/die/schule/ensemble/AlarmActor.scala
@@ -5,6 +5,7 @@ import java.time.LocalDateTime
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
 import die.schule.api.Definition.{Alarm, Alarms}
+import die.schule.service.SomeService
 
 object AlarmActor {
   // actor protocol
@@ -17,13 +18,13 @@ object AlarmActor {
   final case class GetAlarmResponse(maybeAlarm: Option[Alarm])
   final case class CommandResponse(response: String)
 
-  def apply(): Behavior[Command] = performActionOn(List.empty, Map.empty, 0)
+  def apply(someService: SomeService): Behavior[Command] = performActionOn(List.empty, Map.empty, 0, someService)
 
-  private def performActionOn(domainAlarms: List[DomainAlarm], alarmsById: Map[String, Alarm], nextId: Int): Behavior[Command] =
+  private def performActionOn(domainAlarms: List[DomainAlarm], alarmsById: Map[String, Alarm], nextId: Int, someService: SomeService): Behavior[Command] =
     Behaviors.receiveMessage {
       case GetAlarms(replyTo) =>
         val alarms = domainAlarms.map(domainAlarm => Alarm(domainAlarm.id))
-        replyTo ! Alarms(alarms)
+        replyTo ! Alarms(someService.doSomethingMore(alarms))
         Behaviors.same
 
       case CreateAlarm(alarm, replyTo) =>
@@ -32,16 +33,16 @@ object AlarmActor {
           case Alarm(id, None) => DomainAlarm(id)
           case Alarm(id, Some(bytes) ) => DomainAlarm(id = id, bytesPayload = ArrayFiller.createRandomArrayOf(bytes))
         }
-        performActionOn(domainAlarm :: domainAlarms, alarmsById.updated(nextId.toString, alarm), nextId + 1)
+        performActionOn(domainAlarm :: domainAlarms, alarmsById.updated(nextId.toString, alarm), nextId + 1, someService)
 
       case GetAlarm(id, replyTo) =>
         val maybeAlarm = alarmsById.get(id)
-        replyTo ! GetAlarmResponse(maybeAlarm)
+        replyTo ! GetAlarmResponse(maybeAlarm.map(someService.doSomething(_)))
         Behaviors.same
 
       case DeleteAlarm(id, replyTo) =>
         replyTo ! CommandResponse(s"Alarm $id deleted.")
-        performActionOn(domainAlarms.filterNot(_.id == id), alarmsById.removed(id), nextId)
+        performActionOn(domainAlarms.filterNot(_.id == id), alarmsById.removed(id), nextId, someService)
     }
 }
 
---
+++ src/main/scala/die/schule/service/SomeService.scala
@@ -0,0 +1,32 @@
+package die.schule.service
+
+import die.schule.api.Definition.{Alarm, Alarms}
+
+class SomeService(transformer: SomeTransformer) {
+  def doSomething(alarm: Alarm): Alarm = {
+    transformer.transform(alarm)
+  }
+  def doSomethingMore(alarms: List[Alarm]): List[Alarm] = {
+    Thread.sleep(200)
+    alarms.map(doSomething(_))
+  }
+}
+
+object SomeService {
+  def apply(transformer: SomeTransformer): SomeService = new SomeService(transformer)
+}
+
+class SomeTransformer {
+  def transform(alarm: Alarm): Alarm = {
+    Thread.sleep(100)
+    alarm
+  }
+}
+
+object SomeTransformer {
+  def apply(): SomeTransformer = new SomeTransformer()
+}
+
+object Builder {
+  def apply(): SomeService = SomeService(SomeTransformer())
+}
