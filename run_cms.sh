4a03a565b7479431bfac7e8e6b042c00ac02ed4e
Sergio Magnacco <smagnacco@despegar.com>
2021-06-22 15:57:52 -0300

    adding kanela

--- run_cms.sh
+++ run_cms.sh
@@ -2,5 +2,5 @@
 KANELA_JAR_PATH=/home/smagnacco/Downloads
 
 sed -i 's|javaOptions++=Seq("-Xms3096M","-Xmx3096M","-XX:+UnlockExperimentalVMOptions","-XX:+UseZGC")|javaOptions++=Seq()|g' build.sbt
-sbt run -J-javaagent:$KANELA_JAR_PATH/kanela-agent-1.0.7.jar
+sbt run -J-javaagent:$KANELA_JAR_PATH/kanela-agent-1.0.11.jar
 
--- run_zgc.sh
+++ run_zgc.sh
@@ -4,4 +4,4 @@ export JAVA_HOME=/home/smagnacco/programs/langs/jdk-11.0.7+10
 export PATH=${JAVA_HOME}/bin:$PATH
 sed -i 's|javaOptions++=Seq()|javaOptions++=Seq("-Xms3096M","-Xmx3096M","-XX:+UnlockExperimentalVMOptions","-XX:+UseZGC")|g' build.sbt
 
-sbt run -J-javaagent:$KANELA_JAR_PATH/kanela-agent-1.0.7.jar
+sbt run -J-javaagent:$KANELA_JAR_PATH/kanela-agent-1.0.11.jar
